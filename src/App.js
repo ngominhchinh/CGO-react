0d6306da33b0e4d696e3c4452d68d05a224742cd
ngominhchinh <ngominhchinhdhhd5@gmail.com>
2024-07-16 19:23:21 +0700

    Expand

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddStudent from "./pages/AddStudent";
+import Country from "./pages/Country";
+import FCompoent from "./pages/FComponent";
+import Login from "./pages/Login";
+import Student from "./pages/Student";
+import { Link, Route, Routes } from 'react-router-dom'
+import DetailStudent from "./pages/DetailStudent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello</h1>
+      <Link to='students'>Student</Link> | 
+      <Link to='add-students'>AddStudent</Link> | 
+      <Link to='countries'>Country</Link> | 
+      <Link to='f'>FCompoent</Link> | 
+      <hr/>
+      <Routes>
+        <Route path="students" element={<Student />}>
+          <Route path="add" element={<AddStudent />}></Route>
+          <Route path="f" element={<FCompoent />}></Route>
+        </Route>
+        <Route path="add-students" element={<AddStudent />}></Route>
+        <Route path="countries" element={<Country />}></Route>
+        <Route path="detail-student" element={<DetailStudent />}></Route>
+        <Route path="f/:id" element={<FCompoent />}></Route>
+        <Route path="" element={<Login />}></Route>
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+// '/': Login Component
+// 2 input usn, pass, button
+// Ấn nút, kiểm tra: nếu usn & pass = 'admin' => Student.
+
